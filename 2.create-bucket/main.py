e59a33c387140b59425f0fe53a5cf46eff55ac49
anjanpaul <anjanpaul2lrp@gmail.com>
2022-03-02 08:26:45 +0600

    Update main.py

--- 2.create-bucket/main.py
+++ 2.create-bucket/main.py
@@ -7,7 +7,7 @@ s3 = boto3.resource('s3')
 bucket = s3.create_bucket(
     Bucket='cpl-backend-bucket',
     CreateBucketConfiguration={
-        'LocationConstraint': 'us-gov-west-1'|'us-west-1'|'us-west-2'
+        'LocationConstraint': 'us-gov-west-1'
     }
 )
 
